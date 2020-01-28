a82e3ab84b409106f9cdb5255da0d42bbb7c7c8e
HectorZetino <h.rzc.zetino@hotmail.es>
2020-01-27 21:38:32 -0600

    Agregando Modelo Drink

---
+++ Lab1_ED2/Models/Drink.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Lab1_ED2.Models
+{
+    public class Drink : IComparable
+    {
+        public string Name { get; set; }
+        public string Flavor { get; set; }
+        public string Volume { get; set; }
+        public string Price { get; set; }
+        public string Producer { get; set; }
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
