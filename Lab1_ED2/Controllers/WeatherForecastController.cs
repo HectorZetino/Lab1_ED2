bb32e4e53fd9d99ea1a1e709ee0525b2f0cc8b97
Lester Andrés García Aquino <lagarcia2015@gmail.com>
2020-02-01 09:33:36 -0600

    Modificando 'WeatherForecastController'

--- Lab1_ED2/Controllers/WeatherForecastController.cs
+++ Lab1_ED2/Controllers/WeatherForecastController.cs
@@ -11,11 +11,6 @@ namespace Lab1_ED2.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
 
         public WeatherForecastController(ILogger<WeatherForecastController> logger)
@@ -30,8 +25,7 @@ namespace Lab1_ED2.Controllers
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                TemperatureC = rng.Next(-20, 55)
             })
             .ToArray();
         }
